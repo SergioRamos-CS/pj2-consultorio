7cc113d1ef8d509da74c171ef08da329eef0180a
Sérgio Ramos <srramos123@gmail.com>
2025-09-27 14:47:33 -0300

    Adicionado leitor de conteúdo

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-
-navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('nav-menu-visible');
-    const isVisible = navMenu.classList.contains('nav-menu-visible');
-    navToggle.setAttribute('aria-expanded', isVisible);
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu-visible");
+  const isVisible = navMenu.classList.contains("nav-menu-visible");
+  navToggle.setAttribute("aria-expanded", isVisible);
 });
 
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
-    });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  });
 });
+
+function lerTexto() {
+  const texto = document.getElementById("conteudo").innerText;
+  const utterance = new SpeechSynthesisUtterance(texto);
+  utterance.lang = "pt-BR"; // idioma português do Brasil
+  speechSynthesis.speak(utterance);
+}
