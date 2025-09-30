c28d1b75e739a9af3b86ac476542599c933f193e
Sérgio Ramos <srramos123@gmail.com>
2025-09-30 13:16:36 +0000

    Criação da function `query` no database

--- infra/database.js
+++ infra/database.js
@@ -5,7 +5,7 @@ async function query(queryObject) {
   await client, connect();
   const result = await client.query(queryObject);
   await client.end();
-  return result;
+  returm result;
 }
 
 export default {
